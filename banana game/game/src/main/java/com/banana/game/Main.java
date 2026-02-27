4bb6cb0f79adcd214930e10a138e5f7537ef41c6
Chenuratm <chenuratm@gmail.com>
2026-02-28 00:17:13 +0530

    Getters for players model

--- banana game/game/src/main/java/com/banana/game/Main.java
+++ banana game/game/src/main/java/com/banana/game/Main.java
@@ -1,4 +1,9 @@
 package com.banana.game;
 
+import com.banana.game.controller.LoginController;
+
 public class Main {
+    public static void main(String[] args) {
+        new LoginController();
+    }
 }
