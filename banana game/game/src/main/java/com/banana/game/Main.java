04aa44053adb6597d221d25fdff6c185c98eb10c
Chenuratm <chenuratm@gmail.com>
2026-02-28 07:31:44 +0530

    modified main

--- banana game/game/src/main/java/com/banana/game/Main.java
+++ banana game/game/src/main/java/com/banana/game/Main.java
@@ -1,9 +1,9 @@
 package com.banana.game;
 
-import com.banana.game.controller.LoginController;
 
 public class Main {
-    public static void main(String[] args) {
-        new LoginController();
+    public static void main(String[] args)
+    {
+
     }
 }
