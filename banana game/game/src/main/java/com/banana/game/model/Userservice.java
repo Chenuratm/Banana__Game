5e5323440bf0ba0289953501c3700e4273ce2994
Chenuratm <chenuratm@gmail.com>
2026-03-10 13:11:31 +0530

    User serice added

---
+++ banana game/game/src/main/java/com/banana/game/model/Userservice.java
@@ -0,0 +1,23 @@
+package com.banana.game.service;
+
+import com.banana.game.database.DatabaseConnection;
+
+import java.sql.*;
+
+public class UserService {
+
+    public boolean register(String username, String password) {
+
+        try (Connection conn = DatabaseConnection.getConnection()) {
+
+            String sql = "INSERT INTO users (username, password, score) VALUES (?, ?, 0)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.executeUpdate();
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
