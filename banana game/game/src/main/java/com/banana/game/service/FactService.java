2a8558ae1ffe416980ecbbbdb9ebdd32dc36ee85
Chenuratm <chenuratm@gmail.com>
2026-03-12 23:58:03 +0530

    run main throguh login

--- banana game/game/src/main/java/com/banana/game/service/FactService.java
+++ banana game/game/src/main/java/com/banana/game/service/FactService.java
@@ -1,4 +1,26 @@
 package com.banana.game.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class FactService {
+
+    public String getFact() {
+
+        URL url = new URL("http://numbersapi.com/random/trivia");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(conn.getInputStream())
+        );
+
+        String fact = reader.readLine();
+        reader.close();
+
+        return fact;
+
+    }
 }
