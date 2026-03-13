752391265716163f6cc5420770d8a5a581ad416c
Chenuratm <chenuratm@gmail.com>
2026-03-13 21:53:51 +0530

    Added controllers

--- banana game/game/src/main/java/com/banana/game/controller/GameController.java
+++ banana game/game/src/main/java/com/banana/game/controller/GameController.java
@@ -1,4 +1,153 @@
 package com.banana.game.controller;
 
+import com.banana.game.model.BananaQuestion;
+import com.banana.game.service.BananaApiService;
+import com.banana.game.service.FactService;
+import com.banana.game.service.UserService;
+import com.banana.game.session.SessionManager;
+import com.banana.game.view.GameView;
+
+import javax.swing.*;
+import java.util.Random;
+
 public class GameController {
+
+    private GameView view;
+
+    private BananaApiService api = new BananaApiService();
+    private FactService factService = new FactService();
+    private UserService userService = new UserService();
+
+    private BananaQuestion currentQuestion;
+
+    private int score = 0;
+    private int challenge = 0;
+
+    private int time = 60;
+
+    private Timer timer;
+
+    public GameController(GameView view,String username){
+
+        this.view = view;
+
+        SessionManager.login(username);
+
+        view.setUsername(username);
+
+        startTimer();
+
+        nextChallenge();
+
+        view.getLogoutButton().addActionListener(e->logout());
+    }
+
+    private void startTimer(){
+
+        timer = new Timer(1000,e->{
+
+            time--;
+            view.setTimer(time);
+
+            if(time<=0){
+                endGame();
+            }
+
+        });
+
+        timer.start();
+    }
+
+    private void nextChallenge(){
+
+        if(challenge>=6){
+            endGame();
+            return;
+        }
+
+        challenge++;
+
+        view.setChallenge(challenge);
+
+        currentQuestion = api.getQuestion(challenge);
+
+        view.displayQuestion(currentQuestion);
+
+        generateAnswers();
+    }
+
+    private void generateAnswers(){
+
+        Random r = new Random();
+
+        int correct = currentQuestion.getAnswer();
+
+        int[] answers = new int[5];
+
+        int correctIndex = r.nextInt(5);
+
+        for(int i=0;i<5;i++){
+
+            if(i==correctIndex){
+                answers[i]=correct;
+            }else{
+                answers[i]=r.nextInt(10);
+            }
+        }
+
+        view.setAnswers(answers,this::checkAnswer);
+    }
+
+    private void checkAnswer(int selected){
+
+        if(selected == currentQuestion.getAnswer()){
+
+            score += 10;
+            view.setScore(score);
+        }
+
+        showFact();
+    }
+
+    private void showFact(){
+
+        String fact = factService.getFact();
+
+        view.showFact(fact);
+
+        new Timer(10000,e->{
+
+            ((Timer)e.getSource()).stop();
+            view.showFact("");
+            nextChallenge();
+
+        }).start();
+    }
+
+    private void endGame(){
+
+        timer.stop();
+
+        userService.updateScore(SessionManager.getCurrentUser(),score);
+
+        JOptionPane.showMessageDialog(view,
+                "Game Over\nScore: "+score);
+
+        SessionManager.logout();
+
+        view.dispose();
+
+        new LoginController();
+    }
+
+    private void logout(){
+
+        timer.stop();
+
+        SessionManager.logout();
+
+        view.dispose();
+
+        new LoginController();
+    }
 }
--- banana game/game/src/main/java/com/banana/game/controller/LoginController.java
+++ banana game/game/src/main/java/com/banana/game/controller/LoginController.java
@@ -1,5 +1,41 @@
 package com.banana.game.controller;
 
+import com.banana.game.model.Player;
+import com.banana.game.service.UserService;
+import com.banana.game.view.GameView;
+import com.banana.game.view.LoginView;
+
+import javax.swing.*;
+
 public class LoginController {
 
+    private LoginView view = new LoginView();
+    private UserService service = new UserService();
+
+    public LoginController() {
+
+        view.loginButton.addActionListener(e -> login());
+        view.registerButton.addActionListener(e -> new RegisterController());
+    }
+
+    private void login() {
+
+        String username = view.usernameField.getText();
+        String password = new String(view.passwordField.getPassword());
+
+        if (service.login(username, password)) {
+
+            int score = service.getScore(username);
+            Player player = new Player(username, score);
+
+            view.dispose(); // close login window
+
+            GameView gameView = new GameView();
+
+            new GameController(gameView, player.getUsername());
+
+        } else {
+            JOptionPane.showMessageDialog(view, "Invalid credentials");
+        }
+    }
 }
